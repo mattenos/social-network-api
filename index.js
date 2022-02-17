ac7e26deb31fc9e6dca401ce5e91f9e4591fa7fe
Matt Enos <89925779+mattenos@users.noreply.github.com>
2022-02-17 18:02:33 -0500

    Updated connection. Working on API routes.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongodb = require('mongodb').MongoClient;
+// const db = require('./config/connection');
 
 const app = express();
 const port = 3001;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
 // /api/users
-router.route('/').get(getUsers).post(createUser);
+
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser);
+
 
 module.exports = router;
